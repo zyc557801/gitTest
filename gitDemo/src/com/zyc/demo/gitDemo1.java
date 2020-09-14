af10878be65a4e5a312acb93b83a6b8b1bcd105b
yancheng <2271557801@qq.com>
2020-09-14 12:59:25 +0800

    修改gitDemoClass

--- gitDemo/src/com/zyc/demo/gitDemo1.java
+++ gitDemo/src/com/zyc/demo/gitDemo1.java
@@ -8,9 +8,11 @@ public class gitDemo1 {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        //hello test
     }
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
