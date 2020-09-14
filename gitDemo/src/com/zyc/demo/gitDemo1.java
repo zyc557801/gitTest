29f9e1daddb02ad26518aae945782272ce43380e
yancheng <2271557801@qq.com>
2020-09-14 12:50:00 +0800

    把本地工程添加到git

---
+++ gitDemo/src/com/zyc/demo/gitDemo1.java
@@ -0,0 +1,13 @@
+package com.zyc.demo;
+
+public class gitDemo1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+    }
+}
